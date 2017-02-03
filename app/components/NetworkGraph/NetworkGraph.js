bb5720e8b1459f65098f7f86d958231f05129acf
Jasmin Rubinovitz <jasrub@gmail.com>
2017-02-02 20:25:09 -0500

    graph working but ugly

--- app/components/NetworkGraph/NetworkGraph.js
+++ app/components/NetworkGraph/NetworkGraph.js
@@ -4,10 +4,25 @@ import Radium from 'radium';
 import ReactDOM from 'react-dom'
 
 
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+// var tree = d3.tree()
+//     .size([360, 500])
+//     .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+
 var simulation = d3.forceSimulation()
+
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody())
+    .force("charge", function(d) {
+        return d._children ? -d.size / 100 : d.children ? -100 : -30;
+    });
     //.force("center", d3.forceCenter(width / 2, height / 2));
+// var width = 960;
+// var height = 500;
+// var force = d3.layout.force()
+//     .charge(-300)
+//     .linkDistance(50)
+//     .size([width, height]);
 
 // *****************************************************
 // ** d3 functions to manipulate attributes
@@ -16,48 +31,28 @@ var simulation = d3.forceSimulation()
 var enterNode = (selection) => {
     selection.classed('node', true);
 
-    selection.append('circle')
-        .attr("r", 5) //(d) => d.size) set circle size by node size
-        //.attr("fill", (d)=>d.color)
+    selection.append("circle")
+        .attr("r", 10) // change according to node score
+        .attr("fill", function(d) { return color(d.state); })
         .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended));
 
     selection.append('text')
-        .attr("x", (d) => d.size + 5)
+        //.attr("x", (d) => d.size + 5)
         .attr("dy", ".35em")
         .text((d) => d.name);
 };
-function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-}
-
-function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-}
-
-function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-}
-
 
 var updateNode = (selection) => {
-    // .attr("cx", function(d) { return d.x; })
-    //     .attr("cy", function(d) { return d.y; });
-    console.log(selection)
-
-    //selection.attr("transform", (d) => "translate(" + d.x + "," + d.y + ")");
+    selection.attr("transform", (d) => "translate(" + d.x + "," + d.y + ")");
 };
 
 var enterLink = (selection) => {
     selection.classed('link', true)
-        .attr("stroke-width", (d) => d.size);
+        .attr("stroke-width", 1)
+        .attr('stroke', 'black' );//(d) => d.size);
 };
 
 var updateLink = (selection) => {
@@ -74,87 +69,111 @@ var updateGraph = (selection) => {
         .call(updateLink);
 };
 
-var flatten = (root) => {
-    var nodes = [], i = 0;
-    function recurse(node) {
-        if (node.children) node.children.forEach(recurse);
-        if (!node.id) node.id = ++i;
-        nodes.push(node);
-    }
-
-    recurse(root);
-    return nodes
+var dragstarted = (d) => {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
 };
 
-var ticked = function() {
-    link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-}
-
-var getLinks = (flatNodes) =>{
-    var links = []
-    flatNodes.forEach((node)=>{
-        links.push({'source':node.parentId, 'target':node.id})
-    })
-    return links;
+var dragged = (d) => {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
 };
 
-export const NetworkGraph  = React.createClass({
-    propTypes: {
-        rootNode: PropTypes.object,
-    },
+var dragended = (d) => {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+};
+
+
+// *****************************************************
+// ** Graph and App components
+// *****************************************************
+
+var Graph = React.createClass({
     componentDidMount() {
         this.d3Graph = d3.select(ReactDOM.findDOMNode(this.refs.graph));
+        const svg = ReactDOM.findDOMNode(this.refs.svg);
+        const width = svg.clientWidth;
+        const height = svg.clientHeight;
+        simulation.force("center", d3.forceCenter(width / 2, height / 2));
         simulation.on('tick', () => {
-            // after force calculation starts, call updateGraph
-            // which uses d3 to manipulate the attributes,
-            // and React doesn't have to go through lifecycle on each tick
             this.d3Graph.call(updateGraph);
         });
     },
 
     shouldComponentUpdate(nextProps) {
-        const nodes = flatten(nextProps.rootNode);
-        const links = getLinks(nodes);
         this.d3Graph = d3.select(ReactDOM.findDOMNode(this.refs.graph));
-        console.log(nodes);
-        console.log(links);
-
 
         var d3Nodes = this.d3Graph.selectAll('.node')
-            .data(nodes, (node) => node.id);
+            .data(nextProps.nodes, (node) => node.id);
         d3Nodes.enter().append('g').call(enterNode);
         d3Nodes.exit().remove();
         d3Nodes.call(updateNode);
 
         var d3Links = this.d3Graph.selectAll('.link')
-            .data(links, (link) => link.key);
+            .data(nextProps.links, (link) => link.index);
         d3Links.enter().insert('line', '.node').call(enterLink);
         d3Links.exit().remove();
         d3Links.call(updateLink);
 
-        simulation
-            .nodes(d3Nodes)
-            .on("tick", ticked);
+        simulation.nodes(nextProps.nodes);
+        simulation.force("link").links(nextProps.links);
 
-        simulation.force("link")
-            .links(d3Links);
 
         return false;
     },
 
     render() {
         return (
-            <svg width={'100%'} height={'100%'}>
+            <svg width={'100%'} ref='svg'>
                 <g ref='graph' />
             </svg>
+        );
+    }
+});
+
+
+
+export const NetworkGraph  = React.createClass({
+    propTypes: {
+        rootNode: PropTypes.object,
+    },
+    flatten :function(root) {
+        var nodes = [], i = 0;
+        function recurse(node) {
+            if (node.children) node.children.forEach(recurse);
+            if (!node.id) node.id = ++i;
+            nodes.push(node);
+        }
+
+        recurse(root);
+        return {'nodes': nodes, 'links':this.getLinks(nodes)}
+    },
+
+
+    getLinks: function(flatNodes) {
+    var links = []
+    flatNodes.forEach((node)=>{
+        if (node.parentId) {
+            links.push({'source': node.parentId, 'target': node.id})
+        }
+    });
+    return links;
+    },
+
+render() {
+        const nodes = d3.hierarchy(this.props.rootNode);
+        const links = nodes.links();
+        console.log(nodes);
+        console.log(links);
+        const data = this.flatten(this.props.rootNode);
+        console.log(data);
+        return (
+            <div width={'100%'} height={'70%'}>
+                <Graph nodes={data.nodes} links={data.links} />
+            </div>
         );
     }
 });
