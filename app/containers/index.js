6ff9e59899c43436ef5782f38f22f96adb9e52f2
Travis Rich <travis.s.rich@gmail.com>
2017-02-10 15:18:29 -0500

    Add hidden song button

--- app/containers/index.js
+++ app/containers/index.js
@@ -5,3 +5,4 @@ export Login from './Login/Login';
 export NoMatch from './NoMatch/NoMatch';
 export User from './User/User';
 export LearnMore from './LearnMore/LearnMore';
+export Song from './Song/Song';
