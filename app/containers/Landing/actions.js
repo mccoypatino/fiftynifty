addfbf75a1e047d395ef17961e9f562467ffd395
Travis Rich <travis.s.rich@gmail.com>
2017-02-10 12:57:48 -0500

    Use authentication code on signup

--- app/containers/Landing/actions.js
+++ app/containers/Landing/actions.js
@@ -4,6 +4,10 @@
 // All action types are defined as constants. Do not manually pass action
 // types as strings in action creators
 /*--------*/
+export const POST_USER_AUTHENTICATION_LOAD = 'user/POST_USER_AUTHENTICATION_LOAD';
+export const POST_USER_AUTHENTICATION_SUCCESS = 'user/POST_USER_AUTHENTICATION_SUCCESS';
+export const POST_USER_AUTHENTICATION_FAIL = 'user/POST_USER_AUTHENTICATION_FAIL';
+
 export const POST_USER_LOAD = 'user/POST_USER_LOAD';
 export const POST_USER_SUCCESS = 'user/POST_USER_SUCCESS';
 export const POST_USER_FAIL = 'user/POST_USER_FAIL';
@@ -19,6 +23,30 @@ export const GET_USER_FAIL = 'username/GET_USER_FAIL';
 // action objects (e.g. {type:example, payload:data} ) within dispatch()
 // function calls
 /*--------*/
+export function postUserAuthentication(phone, signupCode) {
+	return (dispatch) => {
+		dispatch({ type: POST_USER_AUTHENTICATION_LOAD });
+		return clientFetch('/api/user/authenticate', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				phone: phone,
+				signupCode: signupCode,
+			})
+		})
+		.then((result) => {
+			dispatch({ type: POST_USER_AUTHENTICATION_SUCCESS, result });
+		})
+		.catch((error) => {
+			console.log(error);
+			dispatch({ type: POST_USER_AUTHENTICATION_FAIL, error });
+		});
+	};
+}
+
 export function postUser(name, phone, zipcode, parentId) {
 	return (dispatch) => {
 		dispatch({ type: POST_USER_LOAD });
