c33ed9cd58af9f847cd6e73abdf50b69e350c75e
Travis Rich <travis.s.rich@gmail.com>
2017-02-11 10:36:04 -0500

    Use getComponent in routes so we can chunk pages better

--- app/Routes.js
+++ app/Routes.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Route } from 'react-router';
-import { App, Landing, Leaderboard, Login, NoMatch, User, About, Song } from 'containers';
+// import { App, Landing, Leaderboard, Login, NoMatch, User, About, Song } from 'containers';
+
+function getComponent(component) {
+	return (location, cb)=> {
+		System.import(`containers/${component}/${component}`)
+		.then(function(module) {
+			cb(null, module.default);
+		})
+		.catch(function(error) {
+			throw new Error(`Dynamic page loading failed: ${error}`);
+		});
+	};
+}
 
 export default (
-	<Route component={App}>
-		<Route path="/" component={Landing} />
-		<Route path="/leaderboard" component={Leaderboard} />
-		<Route path="/login" component={Login} />
-		<Route path="/about" component={About} />
-		<Route path="/song" component={Song} />
-		<Route path="/:userId" component={User} />
+	<Route getComponent={getComponent('App')}>
+		<Route path="/" getComponent={getComponent('Landing')} />
+		<Route path="/leaderboard" getComponent={getComponent('Leaderboard')} />
+		<Route path="/login" getComponent={getComponent('Login')} />
+		<Route path="/about" getComponent={getComponent('About')} />
+		<Route path="/song" getComponent={getComponent('Song')} />
+		<Route path="/:userId" getComponent={getComponent('User')} />
 
-		<Route path="*" component={NoMatch} />
+		<Route path="*" getComponent={getComponent('NoMatch')} />
 	</Route>
 );
--- app/containers/index.js
+++ app/containers/index.js
@@ -1,8 +1,8 @@
-export App from './App/App';
-export Landing from './Landing/Landing';
-export Leaderboard from './Leaderboard/Leaderboard';
-export Login from './Login/Login';
-export NoMatch from './NoMatch/NoMatch';
-export User from './User/User';
-export About from './About/About';
-export Song from './Song/Song';
+// export App from './App/App';
+// export Landing from './Landing/Landing';
+// export Leaderboard from './Leaderboard/Leaderboard';
+// export Login from './Login/Login';
+// export NoMatch from './NoMatch/NoMatch';
+// export User from './User/User';
+// export About from './About/About';
+// export Song from './Song/Song';
