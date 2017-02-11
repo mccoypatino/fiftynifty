307c7f9f9de300b7bf8987a46101c71cab832263
Travis Rich <travis.s.rich@gmail.com>
2017-02-11 09:42:04 -0500

    Cleaner youtube embed

--- app/containers/Song/Song.js
+++ app/containers/Song/Song.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import Radium from 'radium';
+import YouTube from 'react-youtube';
 
 let styles;
 
 export const Song = React.createClass({
+	getInitialState() {
+		return {
+			activeLoading: 3,
+		};
+	},
+
+	renderNext: function() {
+		this.setState({ activeLoading: this.state.activeLoading + 1 });
+	},
 
 	render() {
+		const opts = { height: '337px', width: '100%'};
+		const videos = ['b7jv7H4TGXc', 'Z2Gqe3doat0', 'sxr6NFimibY', 'Lju29keIRuc', '6XkTl4LFzCY'];
 		return (
 			<div style={styles.container}>
 				<div style={styles.content}>
-					<iframe style={styles.video} src="https://www.youtube.com/embed/b7jv7H4TGXc" frameBorder="0" allowFullScreen />
-					<iframe style={styles.video} src="https://www.youtube.com/embed/Z2Gqe3doat0" frameBorder="0" allowFullScreen />
-					<iframe style={styles.video} src="https://www.youtube.com/embed/sxr6NFimibY" frameBorder="0" allowFullScreen />
-					<iframe style={styles.video} src="https://www.youtube.com/embed/Lju29keIRuc" frameBorder="0" allowFullScreen />
-					<iframe style={styles.video} src="https://www.youtube.com/embed/6XkTl4LFzCY" frameBorder="0" allowFullScreen />
+					{videos.filter((video, index)=> {
+						return index <= this.state.activeLoading;
+					}).map((video, index)=> {
+						return (
+							<div style={styles.videoWrapper} key={`video-${index}`}>
+								<YouTube videoId={video} opts={opts} onReady={this.renderNext} />
+							</div>
+						);
+					})}
 				</div>
 				
 			</div>
@@ -29,15 +45,14 @@ styles = {
         color: 'white',
         zIndex: 2,
         position: 'relative',
+        minHeight: '100vh',
 	},
 	content: {
 		padding: 'calc(175px + 2em) 0em 3em',
         maxWidth: '600px',
         margin: '0 auto',
 	},
-	video: {
-		width: '100%',
-		height: '337px',
+	videoWrapper: {
 		marginBottom: '2em',
-	}
+	},
 };
