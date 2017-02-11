1a04c2a3c5bd313879e08af000e3c275d30d488c
Travis Rich <travis.s.rich@gmail.com>
2017-02-11 10:10:05 -0500

    Manual lazy loading for youtube videos

--- app/containers/Song/Song.js
+++ app/containers/Song/Song.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import Radium from 'radium';
-import YouTube from 'react-youtube';
+
 
 let styles;
 
 export const Song = React.createClass({
-	getInitialState() {
-		return {
-			activeLoading: 3,
-		};
-	},
-
-	renderNext: function() {
-		this.setState({ activeLoading: this.state.activeLoading + 1 });
+	
+	loadVideo: function(video) {
+		this.setState({ [video]: true });
 	},
 
 	render() {
-		const opts = { height: '337px', width: '100%'};
 		const videos = ['b7jv7H4TGXc', 'Z2Gqe3doat0', 'sxr6NFimibY', 'Lju29keIRuc', '6XkTl4LFzCY'];
 		return (
 			<div style={styles.container}>
 				<div style={styles.content}>
-					{videos.filter((video, index)=> {
-						return index <= this.state.activeLoading;
-					}).map((video, index)=> {
+					{videos.map((video, index)=> {
 						return (
 							<div style={styles.videoWrapper} key={`video-${index}`}>
-								<YouTube videoId={video} opts={opts} onReady={this.renderNext} />
+								<img style={styles.thumbnail} alt={video} src={`https://img.youtube.com/vi/${video}/sddefault.jpg`} onClick={this.loadVideo.bind(this, video)} />
+
+								<div style={styles.playButton}>
+									PLAY
+								</div>
+
+								{this.state[video] &&
+									<iframe style={styles.iframe} src={`https://www.youtube.com/embed/${video}?autoplay=1`} frameBorder="0" allowFullScreen />
+								}
+								
 							</div>
 						);
 					})}
@@ -54,5 +55,37 @@ styles = {
 	},
 	videoWrapper: {
 		marginBottom: '2em',
+		width: '100%',
+		height: '337px',
+		position: 'relative',
+	},
+	thumbnail: {
+		width: '100%',
+		height: '100%',
+		zIndex: 2,
+		position: 'relative',
+		cursor: 'pointer',
+	},
+	playButton: {
+		position: 'auto',
+		height: '48px',
+		width: '68px',
+		position: 'absolute',
+		lineHeight: '48px',
+		textAlign: 'center',
+		top: '148px',
+		left: 'calc(50% - 35px)',
+		backgroundColor: 'rgba(0, 0, 0, 0.75)',
+		borderRadius: '3px',
+		zIndex: 3,
+		pointerEvents: 'none',
+	},
+	iframe: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		zIndex: 5,
+		width: '100%',
+		height: '100%',
 	},
 };
