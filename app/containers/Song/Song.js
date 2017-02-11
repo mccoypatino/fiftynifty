e74c0d42ab5da5de445f51c9aeb370cd808d597d
Travis Rich <travis.s.rich@gmail.com>
2017-02-11 10:18:00 -0500

    Nevermind that wasn't worth it for the youtube vides. Just back to basic embeds

--- app/containers/Song/Song.js
+++ app/containers/Song/Song.js
@@ -1,36 +1,19 @@
 import React from 'react';
 import Radium from 'radium';
 
-
 let styles;
 
 export const Song = React.createClass({
 	
-	loadVideo: function(video) {
-		this.setState({ [video]: true });
-	},
-
 	render() {
-		const videos = ['b7jv7H4TGXc', 'Z2Gqe3doat0', 'sxr6NFimibY', 'Lju29keIRuc', '6XkTl4LFzCY'];
 		return (
 			<div style={styles.container}>
 				<div style={styles.content}>
-					{videos.map((video, index)=> {
-						return (
-							<div style={styles.videoWrapper} key={`video-${index}`}>
-								<img style={styles.thumbnail} alt={video} src={`https://img.youtube.com/vi/${video}/sddefault.jpg`} onClick={this.loadVideo.bind(this, video)} />
-
-								<div style={styles.playButton}>
-									PLAY
-								</div>
-
-								{this.state[video] &&
-									<iframe style={styles.iframe} src={`https://www.youtube.com/embed/${video}?autoplay=1`} frameBorder="0" allowFullScreen />
-								}
-								
-							</div>
-						);
-					})}
+					<iframe style={styles.video} src="https://www.youtube.com/embed/b7jv7H4TGXc" frameBorder="0" allowFullScreen />
+					<iframe style={styles.video} src="https://www.youtube.com/embed/Z2Gqe3doat0" frameBorder="0" allowFullScreen />
+					<iframe style={styles.video} src="https://www.youtube.com/embed/sxr6NFimibY" frameBorder="0" allowFullScreen />
+					<iframe style={styles.video} src="https://www.youtube.com/embed/Lju29keIRuc" frameBorder="0" allowFullScreen />
+					<iframe style={styles.video} src="https://www.youtube.com/embed/6XkTl4LFzCY" frameBorder="0" allowFullScreen />
 				</div>
 				
 			</div>
@@ -46,46 +29,15 @@ styles = {
         color: 'white',
         zIndex: 2,
         position: 'relative',
-        minHeight: '100vh',
 	},
 	content: {
 		padding: 'calc(175px + 2em) 0em 3em',
         maxWidth: '600px',
         margin: '0 auto',
 	},
-	videoWrapper: {
-		marginBottom: '2em',
+	video: {
 		width: '100%',
 		height: '337px',
-		position: 'relative',
-	},
-	thumbnail: {
-		width: '100%',
-		height: '100%',
-		zIndex: 2,
-		position: 'relative',
-		cursor: 'pointer',
-	},
-	playButton: {
-		position: 'auto',
-		height: '48px',
-		width: '68px',
-		position: 'absolute',
-		lineHeight: '48px',
-		textAlign: 'center',
-		top: '148px',
-		left: 'calc(50% - 35px)',
-		backgroundColor: 'rgba(0, 0, 0, 0.75)',
-		borderRadius: '3px',
-		zIndex: 3,
-		pointerEvents: 'none',
-	},
-	iframe: {
-		position: 'absolute',
-		top: 0,
-		left: 0,
-		zIndex: 5,
-		width: '100%',
-		height: '100%',
-	},
+		marginBottom: '2em',
+	}
 };
