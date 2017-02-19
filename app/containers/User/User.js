80f2513521a8156147eb844b56e1082b8f8965c0
lemeb <lemebfr@gmail.com>
2017-02-18 20:20:09 -0500

    small cleaning

--- app/containers/User/User.js
+++ app/containers/User/User.js
@@ -61,14 +61,14 @@ export const User = React.createClass({
 	},
 
 	getParent: function(user) {
-		const userLevel  = user.hierarchyLevel;
+		const userLevel = user.hierarchyLevel;
 		if (user.ancestors) {
 			const parent = user.ancestors.filter((ancestor) => {
 				if (ancestor.hierarchyLevel === (userLevel - 1)) {
 					return ancestor;
 				}
 			});
-			return parent.length>0? parent[0] : false;
+			return parent.length > 0 ? parent[0] : false;
 		}
 		return false;
 	},
@@ -79,7 +79,7 @@ export const User = React.createClass({
 		// const children = user.children || [];
 		const flatCalls = getFlatCalls(user);
 		const score = getScore(user);
-		const shareUrl = 'https://fiftynifty.org/?ref=' + user.id; //When we get nicer urls, adda "getUrl" function
+		const shareUrl = 'https://fiftynifty.org/?ref=' + user.id; // When we get nicer urls, adda "getUrl" function
 		const statesCount = countStates(user);
 		const chartData = [{ name: 'SatesDone', value: statesCount }, { name: 'not Done', value: 50 - statesCount }];
 		const COLORS = ['#cb0027', 'rgba(0,0,0,0)'];
@@ -87,7 +87,7 @@ export const User = React.createClass({
 		const localUser = localUserData && localUserData.length > 1 ? JSON.parse(localUserData) : {};
 		const isLocalUser = localUser.id && localUser.id === user.id;
 		const presentName = isLocalUser ? 'Your' : user.name + "'s";
-		const joinDate = user.createdAt? dateFormat(new Date(user.createdAt), "mmmm dS, yyyy"): "";
+		const joinDate = user.createdAt ? dateFormat(new Date(user.createdAt), 'mmmm dS, yyyy') : '';
 		const parent = this.getParent(user);
 		const callsCount = getPersonalCallsCount(user);
 
@@ -127,7 +127,7 @@ export const User = React.createClass({
 										<div>
 											<div style={styles.repsWrapper}>
 
-												<div style={styles.repsBox} className={"pt-elevation-3"}>
+												<div style={styles.repsBox} className={'pt-elevation-3'}>
 													<div style={styles.sectionTitle}>Your Representatives</div>
 														<div style={styles.centered}>
 															<button role={'button'} style={styles.button} className={'pt-button pt-minimal'} onClick={this.toggleCallDialog}>How to call?</button>
@@ -161,7 +161,8 @@ export const User = React.createClass({
 																	{reps.map((rep, index)=> {
 																		return (
 																			<Representative key={`rep-${index}`} repData={rep} callFunction={this.callFunction} />
-																		);})}
+																		); 
+																	})}
 																	</div>
 																	<div> Unfortunately, you are not fully represented in congress.</div>
 																	<div>You can still join the game by inviting your friends from other states and encouraging them to call</div>
@@ -182,7 +183,7 @@ export const User = React.createClass({
 
 												</div>
 
-											{/*<p style={styles.orCall}>Or you can call <a style={styles.link} href="tel:508-659-9127">(508) 659-9127</a>  and we'll connect you</p>*/}
+											{/* <p style={styles.orCall}>Or you can call <a style={styles.link} href="tel:508-659-9127">(508) 659-9127</a>  and we'll connect you</p> */}
 											{isLocalUser &&
 												<div style={styles.centered}>
 													<button type="button" className={'pt-button pt-minimal pt-icon-log-out pt-intent-danger'} onClick={this.logout}>Logout</button>
@@ -197,8 +198,8 @@ export const User = React.createClass({
 										<div style={styles.userInfo}>
 											<div style={styles.userInfoItem}><b>Joined:</b> {joinDate}</div>
 											{parent && <div style={styles.userInfoItem}><b>Invited by:</b> <Link style={styles.link} to={`/${parent.id}`}>{parent.name}</Link></div>}
-											<div style={styles.userInfoItem}><b>Calls Made:</b>  {callsCount}</div>
-											<div style={styles.userInfoItem}><b>Calls by Family:</b>  {flatCalls ? flatCalls.length: '0'}</div>
+											<div style={styles.userInfoItem}><b>Calls Made:</b> {callsCount}</div>
+											<div style={styles.userInfoItem}><b>Calls by Family:</b> {flatCalls ? flatCalls.length : '0'}</div>
 										</div>
 									</div>
 								</div>
@@ -212,8 +213,8 @@ export const User = React.createClass({
 					<Invite url={shareUrl} />
 				}
 
-				<div id="progress" style = {styles.progressBackground}>
-					<div style = {styles.repsBackgroundSplash}>
+				<div id="progress" style={styles.progressBackground}>
+					<div style={styles.repsBackgroundSplash}>
 						<div style={styles.progressSection}>
 
 							<div style={styles.sectionTitle}>
@@ -223,18 +224,19 @@ export const User = React.createClass({
 							</div>
 
 							<div style={styles.scoreStats}>
-								<span style={{textAlign:'center', fontWeight:'lighter'}}> Score: <span style={styles.score}> {Math.floor(score)}</span></span>
-								<div style={{display:'inline-block', verticalAlign: 'middle', paddingLeft:'2em', paddingTop:'1em'}}>
+								<span style={{ textAlign: 'center', fontWeight: 'lighter' }}> Score: <span style={styles.score}> {Math.floor(score)}</span></span>
+								<div style={{ display: 'inline-block', verticalAlign: 'middle', paddingLeft: '2em', paddingTop: '1em' }}>
 									<PieChart height={200} width={200}>
-										<Pie isAnimationActive={false} data={chartData} innerRadius={70} outerRadius={100} fill="rgba(102, 102, 102, 0.7)"
-											 stroke="none">
-										</Pie>
+										<Pie 
+											isAnimationActive={false} data={chartData} innerRadius={70} outerRadius={100} fill="rgba(102, 102, 102, 0.7)"
+											stroke="none" />
 										<Pie data={chartData} innerRadius={70} outerRadius={100} fill="#82ca9d" stroke="none">
 											{
-												chartData.map((entry, index) => <Cell key={index} fill={COLORS[index % COLORS.length]}/>)
+												chartData.map((entry, index) => <Cell key={index} fill={COLORS[index % COLORS.length]} />)
 											}</Pie>
-										<text x={100} y={100} textAnchor="middle" dominantBaseline="middle"
-											  fill="white" fontWeight="lighter">
+										<text
+											x={100} y={100} textAnchor="middle" dominantBaseline="middle"
+											fill="white" fontWeight="lighter">
 											{statesCount} / 50 states
 										</text>
 									</PieChart>
@@ -282,9 +284,9 @@ export default connect(mapStateToProps)(Radium(User));
 styles = {
 	container: {
 		padding: 'calc(115px + 0.2em) 0em 3em',
-		//maxWidth: '1024px',
+		// maxWidth: '1024px',
 		margin: '0 auto',
-		opacity:'1',
+		opacity: '1',
 	},
 	plainContainer: {
 		maxWidth: '1024px',
@@ -302,7 +304,7 @@ styles = {
 		textAlign: 'center',
 		padding: '1em 0em',
 		color: 'white',
-		letterSpacing:'0.1em',
+		letterSpacing: '0.1em',
 		position: 'relative',
 	},
 	loader: {
@@ -316,33 +318,33 @@ styles = {
 	},
 	sectionTitle: {
 		fontSize: '1.8em',
-		textAlign:'center',
-		letterSpacing:'0.1em',
-		padding:'0.8em 0',
+		textAlign: 'center',
+		letterSpacing: '0.1em',
+		padding: '0.8em 0',
 		fontWeight: 'lighter',
-		color:'white',
+		color: 'white',
 	},
-	score:{
-		fontSize:'3em',
-		textAlign:'center',
-		fontWeight:'bold',
-		paddingLeft:'0.2em'
+	score: {
+		fontSize: '3em',
+		textAlign: 'center',
+		fontWeight: 'bold',
+		paddingLeft: '0.2em'
 	},
 	centered: {
-		textAlign:'center',
+		textAlign: 'center',
 	},
 	repsSectionTitle: {
 		fontSize: '1.9em',
-		textAlign:'center',
-		letterSpacing:'0.1em',
-		paddingBottom:'1em',
+		textAlign: 'center',
+		letterSpacing: '0.1em',
+		paddingBottom: '1em',
 	},
-	progressBackground: {
-		background:"linear-gradient(rgba(28, 67, 90, 0.8),rgba(28, 67, 90, 0.8)), url('static/crowd.jpg') no-repeat center center",
-		backgroundSize:'cover',
-	},
+	// progressBackground: {
+	// 	background:"linear-gradient(rgba(28, 67, 90, 0.8),rgba(28, 67, 90, 0.8)), url('static/crowd.jpg') no-repeat center center",
+	// 	backgroundSize: 'cover',
+	// },
 	graphBackground: {
-		backgroundColor:"#003d59",
+		backgroundColor: '#003d59',
 	},
 	settingsBackground: {
 		backgroundColor: '#0b5577',
@@ -364,52 +366,50 @@ styles = {
 		left: 0,
 	},
 	repsBackgroundSplash: {
-		background: 'linear-gradient(rgba(0,60,88, 0.8),rgba(0,60,88, 0.8))',//'#1c435a',
-
+		background: 'linear-gradient(rgba(0,60,88, 0.8),rgba(0,60,88, 0.8))', //'#1c435a',
 	},
 	repsWrapper: {
 		margin: 'auto',
 		width: '80%',
-		maxWidth:'350px',
-		backgroundColor:'#da022e',
-		opacity:'0.95',
+		maxWidth: '350px',
+		backgroundColor: '#da022e',
+		opacity: '0.95',
 		fontWeight: 'lighter',
 	},
 	repsBox: {
 		textAlign: 'left',
-		color:'white',
-		padding:'1em',
+		color: 'white',
+		padding: '1em',
 		//fontWeight: '200',
 	},
 	orCall: {
-		textAlign:'center',
-		color:'white',
-		padding:'1em',
-		fontWeight:'lighter',
+		textAlign: 'center',
+		color: 'white',
+		padding: '1em',
+		fontWeight: 'lighter',
 	},
-	familySection:{
-		textAlign:'center',
-		paddingTop:'1em',
+	familySection: {
+		textAlign: 'center',
+		paddingTop: '1em',
 	},
-	progressSection:{
-		padding:'2em 0',
-		color:'white',
+	progressSection: {
+		padding: '2em 0',
+		color: 'white',
 	},
-	scoreStats:{
-		textAlign:'center',
-		paddingTop:'2em',
+	scoreStats: {
+		textAlign: 'center',
+		paddingTop: '2em',
 	},
-	button:{
-		color:'white',
+	button: {
+		color: 'white',
 		fontWeight: 'lighter',
 		backgroundColor: '#003d59',
-		letterSpacing:'0.1em',
-		boxShadow:'0 2px #001C2B',
-
+		letterSpacing: '0.1em',
+		boxShadow: '0 2px #001C2B',
 	},
-	link:{
-		color:'#da022e',
-		fontWeight:'bold',
+	link: {
+		color: '#da022e',
+		fontWeight: 'bold',
 	},
 	dialogBox: {
 		maxWidth: '100%',
@@ -419,13 +419,13 @@ styles = {
 		textAlign: 'center',
 	},
 	userInfo: {
-		color:'#000',
-		textAlign:'center',
-		background:'rgba(255, 255, 255, 0.6)',
-		borderRadius:'5px',
+		color: '#000',
+		textAlign: 'center',
+		background: 'rgba(255, 255, 255, 0.6)',
+		borderRadius: '5px',
 		margin: 'auto',
 		display: 'inline-block',
-		lineHeight:'1.5em',
+		lineHeight: '1.5em',
 		padding: '1em',
 	},
 	userInfoItem: {
