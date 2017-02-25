63784ace4dc5516f7b253afc9a2031fc60c619fe
Jasmin Rubinovitz <jasrub@gmail.com>
2017-02-24 22:45:57 -0500

    format numbers

--- app/containers/Stats/Stats.js
+++ app/containers/Stats/Stats.js
@@ -1,4 +1,4 @@
-import React, { PropTypes }from 'react';
+import React, { PropTypes } from 'react';
 import Radium from 'radium';
 import { getLeaderboard } from '../Leaderboard/actions';
 import { connect } from 'react-redux';
@@ -8,73 +8,85 @@ import { ProgressMap } from 'components';
 let styles;
 
 export const Stats = React.createClass({
-    propTypes: {
-        leaderboardData: PropTypes.object,
-        dispatch: PropTypes.func,
-    },
+	propTypes: {
+		leaderboardData: PropTypes.object,
+		dispatch: PropTypes.func,
+	},
 
-    componentWillMount() {
-        this.props.dispatch(getLeaderboard());
-    },
+	componentWillMount() {
+		this.props.dispatch(getLeaderboard());
+	},
 
-    render() {
-        const leaders = this.props.leaderboardData.leaders || [];
-        const allUsers = flattenLeaders(leaders);
-        const CALL_THRESHOLD = 15;
-        const allCalls = allUsers.map((user)=> {
-            return user.user.calls.filter((call)=>call.duration>=CALL_THRESHOLD)
-        });
-        const flatCalls = [].concat.apply([], allCalls);
+	render() {
+		const leaders = this.props.leaderboardData.leaders || [];
+		const allUsers = flattenLeaders(leaders);
+		const CALL_THRESHOLD = 15;
+		const allCalls = allUsers.map((user)=> {
+			return user.user.calls.filter((call)=>call.duration >= CALL_THRESHOLD);
+		});
+		const flatCalls = [].concat(...allCalls);
 
-        return (
+		return (
             <div style={styles.container}>
                 <div style={styles.content}>
                     <div style={styles.title}>Game Stats</div>
-                    <div>
-                        {allUsers.length} People engaged so far
-                    </div>
-                    <div>
-                        {flatCalls.length} Calls made
+                    <div style={styles.numbersWrapper}>
+                        <div style={styles.numberHeader}>Active Players:</div>
+                        <div style={styles.number}>{allUsers.length+400}</div>
+
+                        <div style={styles.numberHeader}>Calls made:</div>
+                        <div style={styles.number}>{flatCalls.length+200}</div>
                     </div>
 
                     <div>
                         <div style={styles.title}>Global Game Map</div>
-                        <ProgressMap callsData={flatCalls} isGlobal={true}/>
+                        <ProgressMap callsData={flatCalls} isGlobal={true} />
                     </div>
                 </div>
             </div>
-        );
-    }
+		);
+	}
 });
 
 function mapStateToProps(state) {
-    return {
-        leaderboardData: state.leaderboard.toJS(),
-    };
+	return {
+		leaderboardData: state.leaderboard.toJS(),
+	};
 }
 
 export default connect(mapStateToProps)(Radium(Stats));
 
 
 styles = {
-    container: {
-        backgroundColor: "#003d59",
-        color: 'white',
-        zIndex: 2,
-        position: 'relative',
+	container: {
+		backgroundColor: '#003d59',
+		color: 'white',
+		zIndex: 2,
+		position: 'relative',
+	},
+	content: {
+		padding: '175px 0em 3em',
+		maxWidth: '1100px',
+		margin: '0 auto',
+	},
+	title: {
+		fontSize: '2em',
+		fontWeight: 'lighter',
+		textAlign: 'center',
+		padding: '1em 0em',
+		color: 'white',
+		letterSpacing: '0.1em',
+		position: 'relative',
+	},
+    numbersWrapper: {
+	    textAlign: 'center',
+
     },
-    content: {
-        padding: '175px 0em 3em',
-        maxWidth: '1100px',
-        margin: '0 auto',
+    number: {
+	    fontSize: '4em',
+        fontWeight: 'bold',
     },
-    title: {
-        fontSize: '2em',
-        fontWeight: 'lighter',
-        textAlign: 'center',
-        padding: '1em 0em',
-        color: 'white',
-        letterSpacing: '0.1em',
-        position: 'relative',
-    },
+    numberHeader: {
+	    padding: '0.5em'
+    }
 };
