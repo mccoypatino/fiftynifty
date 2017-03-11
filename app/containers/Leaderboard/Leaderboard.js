a1bd58074c57760d1a0a0a37cf0d9da440a4a126
Jasmin Rubinovitz <jasrub@gmail.com>
2017-03-10 20:20:08 -0500

    fix leaderboard local user bug

--- app/containers/Leaderboard/Leaderboard.js
+++ app/containers/Leaderboard/Leaderboard.js
@@ -20,76 +20,73 @@ export const Leaderboard = React.createClass({
 		this.props.dispatch(getLeaderboard());
 	},
 
-    getInitialState() {
+	getInitialState() {
 		return {
 			leaderSort: 'byStateCount',
 		};
 	},
 
-    leadersByStateCount() {
-        const leaders = this.props.leaderboardData.leaders || [];
-        const flatLeaders = flattenLeaders(leaders).sort(function (a,b) {
-            return cmp(a.statesCount, b.statesCount) || cmp(a.score, b.score);
-        }).reverse();
-        console.log("called");
-        return flatLeaders;
-    },
+	leadersByStateCount() {
+		const leaders = this.props.leaderboardData.leaders || [];
+		const flatLeaders = flattenLeaders(leaders).sort(function (a, b) {
+			return cmp(a.statesCount, b.statesCount) || cmp(a.score, b.score);
+		}).reverse();
+		return flatLeaders;
+	},
 
-    leadersByScore() {
-        const leaders = this.props.leaderboardData.leaders || [];
-        const flatLeaders = flattenLeaders(leaders).sort(function (a,b) {
-            return cmp(a.score, b.score);
-        }).reverse();
-        return flatLeaders;
-    },
+	leadersByScore() {
+		const leaders = this.props.leaderboardData.leaders || [];
+		const flatLeaders = flattenLeaders(leaders).sort(function (a, b) {
+			return cmp(a.score, b.score);
+		}).reverse();
+		return flatLeaders;
+	},
 
-    leadersByNumChildren() {
-        const leaders = this.props.leaderboardData.leaders || [];
-        const flatLeaders = flattenLeaders(leaders).sort(function (a,b) {
-            const aNum = a.user.children ? a.user.children.length : 0;
-            const bNum = b.user.children ? b.user.children.length : 0;
-            return cmp(aNum, bNum) || cmp(a.score, b.score);
-        }).reverse();
-        return flatLeaders;
-    },
+	leadersByNumChildren() {
+		const leaders = this.props.leaderboardData.leaders || [];
+		const flatLeaders = flattenLeaders(leaders).sort(function (a, b) {
+			const aNum = a.user.children ? a.user.children.length : 0;
+			const bNum = b.user.children ? b.user.children.length : 0;
+			return cmp(aNum, bNum) || cmp(a.score, b.score);
+		}).reverse();
+		return flatLeaders;
+	},
 
-    toggleByStateCount: function() {
+	toggleByStateCount: function() {
 		this.setState({ leaderSort: 'byStateCount' });
 	},
 
-    toggleByScore: function() {
+	toggleByScore: function() {
 		this.setState({ leaderSort: 'byScore' });
 	},
 
-    toggleByNumChildren: function() {
+	toggleByNumChildren: function() {
 		this.setState({ leaderSort: 'byNumChildren' });
 	},
 
 	render() {
-        let leaders = [];
+		let leaders = [];
 
-        const localUserData = localStorage.getItem('userData');
-        const localUser = localUserData && localUserData.length > 1 ? JSON.parse(localUserData) : {};
-        const loggedIn = localUser.id && localUser.hash;
+		const localUserData = localStorage.getItem('userData');
+		const localUser = localUserData && localUserData.length > 1 ? JSON.parse(localUserData) : {};
+		const loggedIn = localUser.id && localUser.hash;
         // !! Please use logged in when trying to display 'myId' - we need to make sure both userId and userHash exist in localstorage.
-        
-        const myId = JSON.parse(localStorage.getItem('userData'))["id"];
-        console.log(myId);
 
-        if (this.state.leaderSort === 'byStateCount') {
-            leaders = this.leadersByStateCount();
-        }
-        else if (this.state.leaderSort === 'byScore') {
-            leaders = this.leadersByScore();
-        }
-        else if (this.state.leaderSort === 'byNumChildren') {
-            leaders = this.leadersByNumChildren();
-        }
-        const myLeaderIndex = leaders.findIndex(function (leader) {
-            console.log(leader.user.id);
-            return leader.user.id === myId;
-        });
-        const myLeader = leaders[myLeaderIndex];
+
+		if (this.state.leaderSort === 'byStateCount') {
+			leaders = this.leadersByStateCount();
+		}		else if (this.state.leaderSort === 'byScore') {
+			leaders = this.leadersByScore();
+		}		else if (this.state.leaderSort === 'byNumChildren') {
+			leaders = this.leadersByNumChildren();
+		}
+		console.log('111');
+        const myLeaderIndex = leaders.findIndex((leader) => leader.user.id === localUser.id );
+        console.log('222');
+		const myLeader = loggedIn ? leaders[myLeaderIndex] : null;
+		console.log("my leader", myLeader);
+		console.log("my index", myLeaderIndex);
+		console.log(loggedIn)
 		return (
 		    <div style={styles.flagImage}>
                 <div style={styles.flagSplash}>
@@ -104,13 +101,14 @@ export const Leaderboard = React.createClass({
                             <button role={'button'} style={styles.sortButton} className={'pt-button count-sort'} onClick={this.toggleByStateCount}>State Count</button>
                             <button role={'button'} style={styles.sortButton} className={'pt-button score-sort'} onClick={this.toggleByScore}>Score</button>
                             <button role={'button'} style={styles.sortButton} className={'pt-button invites-sort'} onClick={this.toggleByNumChildren}>Invites</button>
-                            {myLeader && 
-                                <div style={styles.myLeader}><Leader key={myId} leader={myLeader} index={myLeaderIndex+1}/></div>
+                            {loggedIn && myLeader &&
+                                <div style={styles.myLeader}><Leader key={localUser.id} leader={myLeader} index={myLeaderIndex + 1} /></div>
                             }
-                            {leaders.slice(0,10).map((user, index)=>{
-                                return(
-                                    <Leader key={user.user.id} leader={user} index={index+1}/>)
-                                })}
+                            {(leaders.length>0) && leaders.slice(0, 10).map((user, index)=>{
+                                return (
+
+                                    <Leader key={user.user.id} leader={user} index={index + 1} />);
+                            })}
                         </div>
                     </div>
                 </div>
@@ -120,36 +118,37 @@ export const Leaderboard = React.createClass({
 });
 
 export const Leader = React.createClass({
-    propTypes: {
-        leader: PropTypes.object,
-        index: PropTypes.number,
-    },
-    getInitialState () {
-        return {
-            didRender : false
-        };
-    },
-    componentDidMount() {
-        this.loadTimeout = setTimeout(()=>this.setState({
-            didRender:true
-        }),0);
-    },
+	propTypes: {
+		leader: PropTypes.object,
+		index: PropTypes.number,
+	},
+	getInitialState () {
+		return {
+			didRender: false
+		};
+	},
+	componentDidMount() {
+		this.loadTimeout = setTimeout(()=>this.setState({
+			didRender: true
+		}), 0);
+	},
 
-    componentWillUnmount () {
-        this.loadTimeout && clearInterval(this.loadTimeout);
-        this.loadTimeout = false;
-    },
+	componentWillUnmount () {
+		this.loadTimeout && clearInterval(this.loadTimeout);
+		this.loadTimeout = false;
+	},
 
-    render() {
-        const user = this.props.leader;
-        const percent = this.state.didRender? (Math.min(user.statesCount, 50)/50.0 )*100: 0;
-        return(
+	render() {
+		const user = this.props.leader;
+		const percent = this.state.didRender ? (Math.min(user.statesCount, 50) / 50.0) * 100 : 0;
+        console.log(user, this.props.index);
+		return (
             <div style={styles.leaderRow}>
                 <div style={styles.section}>
-                    <span style={styles.leaderIndex}>{this.props.index}</span><Link style={styles.leaderName} to={`/${user.user.id}`}>{user.user.name}</Link>
+                    <div style={styles.numberCircle}><span style={styles.leaderIndex}>{this.props.index}</span></div><Link style={styles.leaderName} to={`/${user.user.id}`}>{user.user.name}</Link>
                     <div style={styles.statesBar}>
                         <div style={styles.outerBar}>
-                            <div style={styles.progressbar(percent)}></div>
+                            <div style={styles.progressbar(percent)} />
                         </div>
                         <div style={styles.statesCount}>{Math.min(user.statesCount, 50)} / 50</div>
                     </div>
@@ -157,14 +156,14 @@ export const Leader = React.createClass({
                     <div style={styles.numInvites}>{user.user.children ? user.user.children.length : 0} Invites Sent</div>
                 </div>
             </div>
-        )
-    }
+		);
+	}
 });
 
 function cmp(a, b) {
-    if (a > b) return +1;
-    if (a < b) return -1;
-    return 0;
+	if (a > b) return +1;
+	if (a < b) return -1;
+	return 0;
 }
 
 function mapStateToProps(state) {
@@ -176,135 +175,145 @@ function mapStateToProps(state) {
 export default connect(mapStateToProps)(Radium(Leaderboard));
 
 styles = {
-    progressbar: function(percent) {
-        return {
-            width: (100-percent)+'%',
-            height:'1em',
-            background: '#f7f5f3',
-            opacity:'1',
-            position: 'absolute',
-            right: '0px',
-            transition:'width 2s'
+	progressbar: function(percent) {
+		return {
+			width: (100 - percent) + '%',
+			height: '1em',
+			background: '#f7f5f3',
+			opacity: '1',
+			position: 'absolute',
+			right: '0px',
+			transition: 'width 2s'
 
-        };
-    },
+		};
+	},
 	container: {
 		padding: 'calc(115px + 2em) 2em 3em',
 		maxWidth: '1024px',
 		margin: '0 auto',
-        zIndex: 2,
-        position: 'relative',
-        display:'block',
+		zIndex: 2,
+		position: 'relative',
+		display: 'block',
 	},
 	content: {
 		padding: '0.1em',
 	},
-    section: {
-        padding:'1em',
-        color:'#00296a',
-        opacity:'1'
-    },
+	section: {
+		padding: '1em',
+		color: '#00296a',
+		opacity: '1'
+	},
 	title: {
 		fontSize: '2.5em',
 		fontWeight: '200',
 	},
-    statesBar: {
+	statesBar: {
 	    width: '100%',
-        maxWidth:'100%',
-        paddingTop: '1em'
-    },
-    statesCount: {
-        marginRight: '1em',
-        display: 'inline-block',
-        color:'#00296a',
-        fontSize:'1em',
-        fontWeight: '600',
-        float:'right',
-        width:'75px',
-        textAlign: 'right',
-
-    },
-    leaderRow: {
-        background: '#dce4ef',
-        borderBottom: '3px solid rgba(8, 48, 74, 0.5)',
-        opacity:'1',
-    },
-    outerBar: {
-	    width:'calc(100% - 90px)',
-        height:'1em',
-        display: 'inline-block',
-        textAlign:'center',
-        background: 'linear-gradient(to right, #ff6f70, #cf1e34)',
-        position: 'relative',
-        verticalAlign: 'middle'
+		maxWidth: '100%',
+		paddingTop: '1em'
 	},
-    star: {
-        textAlign: 'center',
-        color:'white'
-    },
-    starsTable: {
-        width: '100%',
-        height: '100%',
-        position: 'relative',
-        left: '50%',
-        top: '50%',
-    },
-    leaderName: {
-        fontWeight:'300',
-        fontSize:'1em',
-        fontWeight: 'bold',
-    },
-    leaderIndex:{
-        color: '#ff6f70',
-        fontSize:'2em',
-        fontWeight: 'bold',
-        marginRight: '5px',
-    },
-    myLeader:{
-        marginTop: '5px',
-        marginBottom:'30px',
-    },
-    score:{
-        display: 'inline-block',
-        fontSize:'0.9em',
-        fontWeight:'light',
-        paddingTop: '1em',
-        width: '50%',
-        textAlign: 'center',
-    },
-    numInvites:{
-        display: 'inline-block',
-        fontSize:'0.9em',
-        fontWeight:'light',
-        paddingTop: '1em',
-        width: '50%',
-        textAlign: 'center',
-    },
-    flagImage: {
-        backgroundImage: 'url("/static/american-flag.jpg")',
-        backgroundRepeat: 'repeat',
-        backgroundPosition: 'center center',
-        backgroundSize: 'cover',
-        minHeight:'100vh',
-    },
-    flagSplash: {
-        backgroundColor: '#1c435a',
-        opacity: 0.9,
-        minHeight:'100vh',
-    },
-    centered: {
-        textAlign:'center',
-    },
+	statesCount: {
+		marginRight: '1em',
+		display: 'inline-block',
+		color: '#00296a',
+		fontSize: '1em',
+		fontWeight: '600',
+		float: 'right',
+		width: '75px',
+		textAlign: 'right',
 
-    sortTitle: {
+	},
+	leaderRow: {
+		background: '#dce4ef',
+		borderBottom: '3px solid rgba(8, 48, 74, 0.5)',
+		opacity: '1',
+	},
+	outerBar: {
+	    width: 'calc(100% - 90px)',
+		height: '1em',
+		display: 'inline-block',
+		textAlign: 'center',
+		background: 'linear-gradient(to right, #ff6f70, #cf1e34)',
+		position: 'relative',
+		verticalAlign: 'middle'
+	},
+	star: {
+		textAlign: 'center',
+		color: 'white'
+	},
+	starsTable: {
+		width: '100%',
+		height: '100%',
+		position: 'relative',
+		left: '50%',
+		top: '50%',
+	},
+	leaderName: {
+		fontWeight: '300',
+		fontSize: '1em',
+		fontWeight: 'bold',
+	},
+    numberCircle: {
+        borderRadius: '50%',
+        width: '60px',
+        fontSize: '1.5em',
+        fontWeight: 'bold',
+        border: '4px solid #666',
         display: 'inline-block',
-        color: '#FFFFFF',
-    },
+        marginRight: '1em',
 
-    sortButton: {
-        display: 'inline-block',
-        marginLeft: '5px',
-        marginBottom: '5px',
     },
+	leaderIndex: {
+		color: '#ff6f70',
+        textAlign: 'center',
+        lineHeight: '52px',
+        display: 'block',
+	},
+	myLeader: {
+		marginTop: '5px',
+		marginBottom: '30px',
+	},
+	score: {
+		display: 'inline-block',
+		fontSize: '0.9em',
+		fontWeight: 'light',
+		paddingTop: '1em',
+		width: '50%',
+		textAlign: 'center',
+	},
+	numInvites: {
+		display: 'inline-block',
+		fontSize: '0.9em',
+		fontWeight: 'light',
+		paddingTop: '1em',
+		width: '50%',
+		textAlign: 'center',
+	},
+	flagImage: {
+		backgroundImage: 'url("/static/american-flag.jpg")',
+		backgroundRepeat: 'repeat',
+		backgroundPosition: 'center center',
+		backgroundSize: 'cover',
+		minHeight: '100vh',
+	},
+	flagSplash: {
+		backgroundColor: '#1c435a',
+		opacity: 0.9,
+		minHeight: '100vh',
+	},
+	centered: {
+		textAlign: 'center',
+	},
+
+	sortTitle: {
+		display: 'inline-block',
+		color: '#FFFFFF',
+	},
+
+	sortButton: {
+		display: 'inline-block',
+		marginLeft: '5px',
+		marginBottom: '5px',
+	},
 
 };
